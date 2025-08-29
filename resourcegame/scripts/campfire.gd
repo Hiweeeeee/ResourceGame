d056372ee72726500e3ad1764b3260c5ab62a4e7
Weeeee <lucienackley@gmail.com>
2025-08-28 22:08:21 -0700

    I started adding light to the game, grabbing sticks and putting them in the campfire will might the light bigger and brighter

--- resourcegame/scripts/campfire.gd
+++ resourcegame/scripts/campfire.gd
@@ -1,5 +1,10 @@
 extends Node2D
+
+
 @onready var game_manager = %GameManager
+@onready var light = $Light
+
+
 
 var player_in_fire = false
 
@@ -11,5 +16,7 @@ func _on_area_2d_body_exited(body):
 	player_in_fire = false
 
 func _process(delta):
-	if Input.is_action_just_pressed("interact") and player_in_fire == true:
+	if Input.is_action_just_pressed("interact") and player_in_fire == true and game_manager.sticks != 0:
 		game_manager.put_down_stick()
+		light.energy += .5
+		light.texture_scale += .2
